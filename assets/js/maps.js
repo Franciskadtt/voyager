f85a6dd8c0c40037869f9f8b5a0f7c66563975fc
Franciskadtt <franciskadtt@gmail.com>
2021-03-08 16:45:54 +0000

    Add google map to store section

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,29 +1,33 @@
 
 /** Declare a function to create a map - original code https://developers.google.com/maps/documentation/javascript/marker-clustering#maps_marker_clustering-javascript with modifications for project */
+
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
-        center: {
-            lat: 53.349804,
-            lng: -6.260310
-        }
+    const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 8,
+    center: { lat: 53.349804, lng: -6.260310 },
     });
 
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    var locations = [
-        { lat: 53.721160, lng: -4.139210},
-        { lat: 53.283400, lng: -6.120070},
-        { lat: 53.344100, lng: -6.267490},
-        { lat: 53.2941696, lng: -6.133857},
-    ];
-
-    var markers = locations.map(function(location, i) {
-        return new google.maps.Marker({
-            position: location,
-            label: labels[i % labels.length]
-        });
+    /**  Create an array of alphabetical characters used to label the markers.*/
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    /** Add markers to the map.*/
+    const markers = locations.map((location, i) => {
+    return new google.maps.Marker({
+        position: location,
+        label: labels[i % labels.length],
     });
-
-    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+    });
+    /** Add a marker clusterer to manage the markers.*/
+    new MarkerClusterer(map, markers, {
+    imagePath:
+        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+    });
 }
+    /** Add marker locations */
+    let locations = [
+        { lat: 53.2951569, lng: -6.1384571 },
+        { lat: 53.3925976, lng: -6.068269 },
+        { lat: 53.291892, lng: -6.1350469 },
+        { lat: 53.2941696, lng: -6.133857 },
+];
+
+google.maps.event.addListener(markers, "click", function() {})
