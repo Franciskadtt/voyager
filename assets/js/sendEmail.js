b9b6530896a761cb70beacd0b82a43f5689f7bc1
Franciskadtt <franciskadtt@gmail.com>
2021-03-18 09:12:22 +0000

    Fix timing of user message and reset for feedback form

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -11,25 +11,24 @@ function sendMail(contactForm) {
         
         function(response) {
             console.log("SUCCESS", response);
+            //display message to user if succesful
+            reviewMessage ();
+            //reser form
+            resetForm();
         },
         function(error) {
             console.log("FAILED", error);
         }
         
     );
-    resetForm
-    return false;  // To block from loading a new page
+    // To block from loading a new page
+    return false;  
 };
 
-//Listen for click of "send message" button and execute alert
-document.getElementById("feedback-btn").addEventListener("click", reviewMessage);
 // function for alert for feedback form
 function reviewMessage (){
     alert("Thanks for your feedback")   
 }
-
-// Listen for click of "send message" button and execute 
-document.getElementById("feedback-btn").addEventListener("click", resetForm)
 
 // function reset form
 function resetForm() {
