5bd879dec091fd0edefb376ee6598fa5ba2d2908
Franciskadtt <franciskadtt@gmail.com>
2021-03-18 09:33:18 +0000

    Add feature to map markers to hide and display store info on click

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,38 +1,45 @@
 
-/** Declare a function to create a map - original code https://developers.google.com/maps/documentation/javascript/marker-clustering#maps_marker_clustering-javascript with modifications for project */
+// Function display and hide store info if click on marker
+function displayMarkerInfo(){
+    /** add class to hide all elements with store class - orginal code from: https://stackoverflow.com/questions/22270664/how-to-remove-a-class-from-elements-in-pure-javascript */
+    var elems = document.querySelectorAll(".store");
+    [].forEach.call(elems, function(el) {
+        el.classList.add("hide");
+    });
+    // Remove class to display element
+    document.getElementById(this.storeId).classList.remove('hide');
+}
 
+/** Declare a function to create a map - original code https://developers.google.com/maps/documentation/javascript/marker-clustering#maps_marker_clustering-javascript with modifications for project */
 function initMap() {
     const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 10,
-    center: { lat: 53.349804, lng: -6.260310 },
+        zoom: 10,
+        center: { lat: 53.349804, lng: -6.260310 },
     });
 
-    /** Add markers to the map.*/
-    var markers = locations.map((location, i) => {
-    return new google.maps.Marker({
-        position: location,
-        
+    //Add marker locations and storeIds
+    var markerList = [
+        {'location': { lat: 53.2951569, lng: -6.1384571 }, 'storeId': 'downersails'},
+        {'location': { lat: 53.3925976, lng: -6.068269 }, 'storeId': 'marinesuppliers'},
+        {'location': { lat: 53.291892, lng: -6.1350469 }, 'storeId': 'trespass'},
+        {'location': { lat: 53.2941696, lng: -6.133857 }, 'storeId': 'vikingmarine'},
+    ];
+
+    // Add markers to the map and link to position and storeId
+    var markers = markerList.map((marker, i) => {
+        newMarker= new google.maps.Marker({
+            position: marker.location,
+            storeId: marker.storeId
+            
+        });
+        // when marker clicked then display info
+        newMarker.addListener("click", displayMarkerInfo);
+        return newMarker;
     });
-    });
-    /** Add a marker clusterer to manage the markers.*/
+
+    // Add a marker clusterer to manage the markers
     new MarkerClusterer(map, markers, {
-    imagePath:
-        "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+        imagePath:
+            "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
     });
 }
-    /** Add marker locations */
-    var locations = [
-        { lat: 53.2951569, lng: -6.1384571 },
-        { lat: 53.3925976, lng: -6.068269 },
-        { lat: 53.291892, lng: -6.1350469 },
-        { lat: 53.2941696, lng: -6.133857 },
-];
-    /** Add function show store info*/
-function storeDetails() {
-    var showInfo = 
-
-    markers.addListener("click", function() {
-        infowWindow.display(map,markers);
-    });
-}
-
